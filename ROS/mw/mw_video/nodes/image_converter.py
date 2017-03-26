5d42d46c94fcde26dd0382fc7c9f76679b200c02
Marco Walther <marco@sonic.net>
2017-03-26 13:57:17 -0700

    More debug help: publish a image topic and add the process times to it

--- ROS/mw/mw_video/nodes/image_converter.py
+++ ROS/mw/mw_video/nodes/image_converter.py
@@ -15,6 +15,9 @@ class cvBridgeFindCone():
     def __init__(self):
         self.node_name = "mwFindCone"
         self.time_micros = time.time()
+        self.processing_time = 0
+        self.average_processing_time = 0
+        self.average_time = 0
         self.count = 0
         self.distance = 0
         self.delta_heading = 0
@@ -28,14 +31,20 @@ class cvBridgeFindCone():
         
         # What we do during shutdown
         rospy.on_shutdown(self.cleanup)
-        
+
+        self.is_debug = rospy.get_param('~debug', False)
+        rospy.loginfo('%s: Parameter %s has value %s', self.node_name, rospy.resolve_name('~debug'), self.is_debug)
+
         # Create the cv_bridge object
         self.bridge = CvBridge()
-        
+
         self.image_sub = rospy.Subscriber("/camera/rgb/image_raw", Image, self.image_callback, queue_size = 1)
-        self.image_pub = rospy.Publisher("/mw/image_topic", Image, queue_size = 1)
         self.cone_pub = rospy.Publisher("/mw/cone_location", ConeLocation, queue_size = 1)
-        
+        self.image_pub = None
+
+        if self.is_debug:
+            self.image_pub = rospy.Publisher("/mw/image_topic", Image, queue_size = 1)
+
         rospy.loginfo("%s: Ready." % self.node_name)
 
     def image_callback(self, ros_image):
@@ -52,12 +61,15 @@ class cvBridgeFindCone():
             return
 
         # Process the frame using the process_image() function
+        t = time.time()
         display_image = self.process_image(frame)
-                       
-        # try:
-        #     self.image_pub.publish(self.bridge.cv2_to_imgmsg(display_image, "bgr8"))
-        # except CvBridgeError, e:
-        #     print e
+        self.processing_time += time.time() - t
+
+        if self.is_debug: 
+            try:
+                self.image_pub.publish(self.bridge.cv2_to_imgmsg(display_image, "bgr8"))
+            except CvBridgeError, e:
+                print e
 
         if self.found:
             self.found = False
@@ -66,8 +78,11 @@ class cvBridgeFindCone():
             self.cone_pub.publish(cl)
 
         if self.count % 100 == 0:
-            current_micros = time.time();
-            rospy.loginfo("%s: time: %f\n" % (self.node_name, ((current_micros  - self.time_micros) / 100)));
+            current_micros = time.time()
+            self.average_time =(current_micros  - self.time_micros) / 100
+            self.average_processing_time = self.processing_time / 100
+            self.processing_time = 0
+            rospy.loginfo("%s: time: %f  %f\n" % (self.node_name, self.average_time, self.average_processing_time))
             self.time_micros = current_micros
 
     def process_image(self, frame):
@@ -93,6 +108,9 @@ class cvBridgeFindCone():
         elif len(ret) == 2:
             contours = ret[0]
 
+        if self.is_debug:
+            t = "Avg Time: %6.4f (%6.4f)" % (self.average_time, self.average_processing_time)
+
         if contours != None:
             areas = [cv2.contourArea(c) for c in contours]
 
@@ -106,10 +124,16 @@ class cvBridgeFindCone():
                 if w * h > 160 and w / h < 1.0:
                     self.distance = 0.24 * self.frame_height / (0.3969 * h)
                     self.delta_heading = (70.42 / self.frame_width) * ((x + w / 2) - (self.frame_width / 2))
-                    # cv2.drawContours(frame, [poly], 0, (12, 236, 160), 1)
-                    # cv2.rectangle(frame, (x, y), (x + w, y + h), (12, 236, 160), 2)
                     self.found = True
-        
+
+                    if self.is_debug:
+                        cv2.drawContours(frame, [poly], 0, (12, 236, 160), 1)
+                        cv2.rectangle(frame, (x, y), (x + w, y + h), (12, 236, 160), 2)
+                        t += " Dist: %6.2fm   Head: %6.2f" % (self.distance, self.delta_heading)
+
+        if self.is_debug:
+            cv2.putText(frame, t, (10, 350), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 60, 245), 2)
+
         return frame
     
     def cleanup(self):
