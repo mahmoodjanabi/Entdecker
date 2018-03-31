9f06d81f7061fd0d5f4a83cf5be0bf0316564b47
Marco Walther <marco@sonic.net>
2018-03-31 16:45:34 -0700

    Update for PyQt5

--- Laptop/GPS/status_display.py
+++ Laptop/GPS/status_display.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 import subprocess
 from threading import Thread
 import time
